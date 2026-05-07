83fad4a8c65d1eb87d76425ea267be23e24e59dd
leegt <jimmy658437@gmail.com>
2026-05-07 22:28:21 +0800

    Initial Commit

---
+++ HW #5 Multiple Choice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HW #5 Multiple Choice
+//
+//  Created by 114-2Workshop12 on 2026/5/7.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HW #5 Multiple Choice/HW__5_Multiple_ChoiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  HW__5_Multiple_ChoiceApp.swift
+//  HW #5 Multiple Choice
+//
+//  Created by 114-2Workshop12 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct HW__5_Multiple_ChoiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
